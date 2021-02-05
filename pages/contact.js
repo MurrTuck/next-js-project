522126a826ca7335cf2a29a5a085786302c66566
MurrTuck <murrtuck@gmail.com>
2021-02-04 18:09:30 -0700

    Starting the Header

---
+++ pages/contact.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const contact = () => {
+    return (
+        <div>
+            <Head>
+                <title>Contact Us Page</title>
+                <meta name='keywords' content='Contact Us Page, Dont be a stranger' />
+            </Head>
+            <h1>Contact Us</h1>
+        </div>
+    )
+}
+
+export default contact
