7cd636fb03497497a7d737acd09ef26c0c6d1e4b
MurrTuck <murrtuck@gmail.com>
2021-02-04 17:32:29 -0700

    Adding Head, Title, Meta

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,12 @@
+import Head from 'next/head'
+
 const about = () => {
     return (
         <div>
+            <Head>
+                <title>About Page</title>
+                <meta name='keywords' content='About Page, All about us Page' />
+            </Head>
             <h1>About</h1>
         </div>
     )
